06daba57264e97781dbf5b7cb779ed3d5271356a
sayshi <s.shirre@hotmail.com>
2022-01-02 17:18:15 +0100

    Add smooth scroll

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 import { FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin } from 'react-icons/fa';
 import { FooterContainer, FooterWrap, FooterLinksContainer,
     FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink, 
 SocialMedia, SocialMediaWrap, SocialLogo, WebsiteRights, SocialIcons, SocialIconLink} from './footer-elements';
 
 const Footer = () => {
+    const toggleHome = () => {
+        scroll.scrollToTop();
+    }
+
     return (
         <FooterContainer>
             <FooterWrap>
@@ -43,7 +48,7 @@ const Footer = () => {
                 </FooterLinksContainer>
                 <SocialMedia>
                     <SocialMediaWrap>
-                        <SocialLogo to='/'>
+                        <SocialLogo to='/' onClick={ toggleHome }>
                             dolla
                             </SocialLogo>
                             <WebsiteRights>
