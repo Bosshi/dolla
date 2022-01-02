84ff778aba3015ca422050be198eba1a053bfb12
sayshi <s.shirre@hotmail.com>
2022-01-02 18:59:48 +0100

    Add linkedin link

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -71,7 +71,7 @@ const Footer = () => {
                                     aria-label='Twitter'>
                                         <FaTwitter/>
                                     </SocialIconLink>
-                                    <SocialIconLink href='/' target='_blank'
+                                    <SocialIconLink href='//www.linkedin.com' target='_blank'
                                     aria-label='Linkedin'>
                                         <FaLinkedin/>
                                     </SocialIconLink>
